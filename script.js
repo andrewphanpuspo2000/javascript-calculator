4b312386cdc331b2d2c152eff3519ffecead9c34
Andrewphanpuspo <andrewphanpuspo2000@gmail.com>
2023-05-15 11:25:53 +1000

    calculator

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
-const hovercolor = document.querySelectorAll(".wrapper > div");
+const getElement = document.querySelectorAll(".wrapper > .btn ");
+let showinput = document.getElementById("show");
+let stringDisplay = "";
+let lastSign = "";
 
-hovercolor.forEach((item) => {
+const audio = new Audio("./aa.wav");
+
+const arrOperator = ["-", "+", "/", "*"];
+getElement.forEach((item) => {
   item.addEventListener("mouseover", () => {
     item.style.background = "red";
   });
   item.addEventListener("mouseout", () => {
     item.style.background = "blue";
   });
+  if (item.innerText != "AC" && item.innerText != "C") {
+    item.addEventListener("click", () => {
+      if (arrOperator.includes(item.innerText) && !stringDisplay.length) {
+        return;
+      }
+
+      if (arrOperator.includes(item.innerText)) {
+        lastSign = item.innerText;
+        console.log("stringtodisplay:", stringDisplay);
+        console.log("LatestOpeartor:", lastSign);
+        const lastCharacter = stringDisplay.slice(-1);
+        console.log("lastcharacter:", lastCharacter);
+
+        // if (arrOperator.includes(lastSign)) {
+        //   stringDisplay = stringDisplay.slice(0, -1);
+        // }
+      }
+      if (item.innerText === "=") {
+        const lastCharacter = stringDisplay.slice(-1);
+        if (arrOperator.includes(lastCharacter)) {
+          stringDisplay = stringDisplay.slice(0, -1);
+        }
+        let result = Math.round(eval(stringDisplay)).toString();
+        stringDisplay = result;
+        audio.play();
+      }
+      if (item.innerText != "=") stringDisplay += item.innerText;
+      displayString(stringDisplay);
+    });
+  }
+  let deleteinput = document.getElementById("delete");
+
+  deleteinput.addEventListener("click", deleteAll);
+
+  let deleteLast = document.querySelector(".c");
+
+  deleteLast.addEventListener("click", deleteLastIndex);
 });
-console.log(hovercolor);
+
+function deleteLastIndex() {
+  stringDisplay = stringDisplay.slice(0, -1);
+  showinput.innerText = stringDisplay;
+}
+
+function deleteAll() {
+  console.log("test", showinput);
+  stringDisplay = "";
+  showinput.innerText = stringDisplay || 0;
+}
+
+const displayString = (val) => {
+  showinput.innerText = val || 0;
+};
+
+// let showinput = document.getElementById("show");
+// const displayVal = document.querySelectorAll("input");
+
+// displayVal.forEach((itm) => {
+//   itm.addEventListener("click", () => {
+//     showinput.innerHTML += itm.value;
+//   });
+// });
+// console.log(displayVal);
